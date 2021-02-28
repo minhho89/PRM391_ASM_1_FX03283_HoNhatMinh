315cf27803670439b3880ada9320908679d21eb7
minhho89 <61931543+minhho89@users.noreply.github.com>
2021-02-28 09:49:28 +0700

    POJO finish

---
+++ app/src/main/java/minhfx03283/funix/prm391_asm_1/QuizType1.java
@@ -0,0 +1,23 @@
+package minhfx03283.funix.prm391_asm_1;
+
+import java.util.Set;
+
+/**
+ * This question type is for single choice option (radioButtons will be displayed in UI).
+ * mOptionsList is a set of multiple choice questions for users to pick.
+ */
+public class QuizType1 extends Quiz {
+    private Set<String> mOptionList;
+
+    public QuizType1() {
+    }
+
+    public Set<String> getOptionList() {
+        return mOptionList;
+    }
+
+    public void setOptionList(Set<String> mOptionList) {
+        this.mOptionList = mOptionList;
+    }
+
+}

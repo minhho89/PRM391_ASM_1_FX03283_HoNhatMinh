8ca3837743a01d13cca5e8e0f10d0ae750a34d92
minhho89 <61931543+minhho89@users.noreply.github.com>
2021-02-28 15:17:14 +0700

    successfully bind adapter to view -> need to fix duplicate

--- app/src/main/java/minhfx03283/funix/prm391_asm_1/QuizRecyclerAdapter.java
+++ app/src/main/java/minhfx03283/funix/prm391_asm_1/QuizRecyclerAdapter.java
@@ -1,7 +1,6 @@
 package minhfx03283.funix.prm391_asm_1;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,15 +14,13 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import org.w3c.dom.Text;
-
 import java.util.List;
 
 import static minhfx03283.funix.prm391_asm_1.QuizRecyclerViewType.LAYOUT_1;
 import static minhfx03283.funix.prm391_asm_1.QuizRecyclerViewType.LAYOUT_2;
 import static minhfx03283.funix.prm391_asm_1.QuizRecyclerViewType.LAYOUT_3;
 
-public class QuizRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class QuizRecyclerAdapter extends RecyclerView.Adapter {
     Context context;
     // List of quizzes to feed data
     List<Quiz> quizzes;
@@ -65,51 +62,46 @@ public class QuizRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
         final String TAG = "Quizzes iterator: ";
         Quiz quiz = quizzes.get(position);
 
-        Log.d(TAG, "onBindViewHolder: ************************");
-        Log.d("Quizzes", "onBindViewHolder: QuizId: "
-                + quiz.getId() + " Type: " + viewTypes.get(position).getViewType());
-        Log.d(TAG, "onBindViewHolder: ************************");
-        switch (viewTypes.get(position).getViewType()) {
-            case LAYOUT_1:
+        if (quiz instanceof QuizType1) {
             // Views of Type 1 quiz
-                if (quiz instanceof QuizType1) {
-                    ViewHolderType1 holderType1 = (ViewHolderType1) holder;
-                    QuizType1 quizType1 = (QuizType1)quiz;
-                    holderType1.setTvQuestion(quizType1.getQuiz());
+            if (quiz instanceof QuizType1) {
+                ViewHolderType1 holderType1 = (ViewHolderType1) holder;
+                QuizType1 quizType1 = (QuizType1) quiz;
+                holderType1.setTvQuestion(quizType1.getQuiz());
 
-                    for (String s : quizType1.getOptionList()) {
-                        RadioButton rb = new RadioButton(
-                                holderType1.getLinearLayout().getContext());
-                        rb.setText(s);
-                        holderType1.getRdOptions().addView(rb);
-                    }
+                for (String s : quizType1.getOptionList()) {
+                    RadioButton rb = new RadioButton(
+                            holderType1.getLinearLayout().getContext());
+                    rb.setText(s);
+                    holderType1.getRdOptions().addView(rb);
                 }
-                break;
-            case LAYOUT_2:
-                // Views of Type 2 quiz
-                if (quiz instanceof QuizType2) {
-                    ViewHolderType2 holderType2 = (ViewHolderType2) holder;
-                    QuizType2 quizType2 = (QuizType2)quiz;
-                    holderType2.setTvQuestion(quizType2.getQuiz());
-
-                    for (String s : quizType2.getOptionList()) {
-                        CheckBox cb = new CheckBox(
-                                holderType2.getLinearLayout().getContext());
-                        cb.setText(s);
-                        holderType2.getLinearLayout().addView(cb);
-                    }
-                }
-                break;
-            case LAYOUT_3:
-                if (quiz instanceof QuizType3) {
-                    ViewHolderType3 holderType3 = (ViewHolderType3) holder;
-                    QuizType3 quizType3 = (QuizType3) quiz;
-                    holderType3.setTvQuestion(quizType3.getQuiz());
-                }
-                break;
-            default: return;
+            }
         }
+
+        if (quiz instanceof QuizType2) {
+            // Views of Type 2 quiz
+            if (quiz instanceof QuizType2) {
+                ViewHolderType2 holderType2 = (ViewHolderType2) holder;
+                QuizType2 quizType2 = (QuizType2) quiz;
+                holderType2.setTvQuestion(quizType2.getQuiz());
+
+                for (String s : quizType2.getOptionList()) {
+                    CheckBox cb = new CheckBox(
+                            holderType2.getLinearLayout().getContext());
+                    cb.setText(s);
+                    holderType2.getLinearLayout().addView(cb);
+                }
+            }
+        }
+
+        if (quiz instanceof QuizType3) {
+            ViewHolderType3 holderType3 = (ViewHolderType3) holder;
+            QuizType3 quizType3 = (QuizType3) quiz;
+            holderType3.setTvQuestion(quizType3.getQuiz());
+        }
+
     }
+
     @Override
     public int getItemCount() {
         return quizzes.size();
@@ -118,16 +110,9 @@ public class QuizRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
 
     @Override
     public int getItemViewType(int position) {
-            switch (viewTypes.get(position).getViewType()) {
-                case 0:
-                    return LAYOUT_1;
-                case 1:
-                    return LAYOUT_2;
-                case 3:
-                    return LAYOUT_3;
-                default:
-                    return -1;
-            }
+        if (quizzes.get(position) instanceof QuizType1) return LAYOUT_1;
+        if (quizzes.get(position) instanceof QuizType2) return LAYOUT_2;
+        return LAYOUT_3;
     }
 
     public class ViewHolderType1 extends RecyclerView.ViewHolder {
@@ -142,18 +127,14 @@ public class QuizRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
             rdOptions = (RadioGroup) itemView.findViewById(R.id.rg_answers);
         }
 
+        public TextView getTvQuestion() {
+            return tvQuestion;
+        }
+
         // Method to set value of views that will be
         // used further in onBindViewHold method.
         private void setTvQuestion(String text) {
             tvQuestion.setText(text);
-        }
-
-        public void setLinearLayout(LinearLayout linearLayout) {
-            this.linearLayout = linearLayout;
-        }
-
-        public TextView getTvQuestion() {
-            return tvQuestion;
         }
 
         public void setTvQuestion(TextView tvQuestion) {
@@ -171,6 +152,10 @@ public class QuizRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
         public LinearLayout getLinearLayout() {
             return linearLayout;
         }
+
+        public void setLinearLayout(LinearLayout linearLayout) {
+            this.linearLayout = linearLayout;
+        }
     }
 
     public class ViewHolderType2 extends RecyclerView.ViewHolder {
@@ -182,9 +167,6 @@ public class QuizRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
             linearLayout = (LinearLayout) itemView.findViewById(R.id.linear_layout_type_2);
             tvQuestion = (TextView) itemView.findViewById(R.id.tv_question);
         }
-        private void setTvQuestion(String text) {
-            tvQuestion.setText(text);
-        }
 
         public LinearLayout getLinearLayout() {
             return linearLayout;
@@ -196,6 +178,10 @@ public class QuizRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
 
         public TextView getTvQuestion() {
             return tvQuestion;
+        }
+
+        private void setTvQuestion(String text) {
+            tvQuestion.setText(text);
         }
 
         public void setTvQuestion(TextView tvQuestion) {
@@ -207,17 +193,12 @@ public class QuizRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
         private LinearLayout linearLayout;
         private TextView tvQuestion;
         private EditText etAnswer;
+
         public ViewHolderType3(@NonNull View itemView) {
             super(itemView);
             linearLayout = (LinearLayout) itemView.findViewById(R.id.linear_layout_type_3);
             tvQuestion = (TextView) itemView.findViewById(R.id.tv_question);
             etAnswer = (EditText) itemView.findViewById(R.id.et_user_answer);
-        }
-        private void setTvQuestion(String text) {
-            tvQuestion.setText(text);
-        }
-        private void setEtAnswer(String text) {
-            etAnswer.setText(text);
         }
 
         public LinearLayout getLinearLayout() {
@@ -232,12 +213,20 @@ public class QuizRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
             return tvQuestion;
         }
 
+        private void setTvQuestion(String text) {
+            tvQuestion.setText(text);
+        }
+
         public void setTvQuestion(TextView tvQuestion) {
             this.tvQuestion = tvQuestion;
         }
 
         public EditText getEtAnswer() {
             return etAnswer;
+        }
+
+        private void setEtAnswer(String text) {
+            etAnswer.setText(text);
         }
 
         public void setEtAnswer(EditText etAnswer) {
